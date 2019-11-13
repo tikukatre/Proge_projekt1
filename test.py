b27e89f6278da92b026577cf1b6956c03c0cc4b6
Katre <tikukatre@gmail.com>
2019-11-13 11:45:58 +0200

    Muudatus 1

--- test.py
+++ test.py
@@ -8,5 +8,20 @@ def summa(a, b):
         print("Ebaõnnestus")
     f.close()
 s=input("Sisestage summa: ")
+
 kp=input("Sisestage kuupäev: ")
 summa(s,kp)
+f=open("summa.txt", "r")
+for i in f:
+    n=i.split(";")
+    kuupäev=n[1]
+    print(kuupäev)
+#    k=kuupäev.split(".")
+#    kup=[] #kuupäevad listi elementidena
+#    for j in k:
+#        kup.append(j)
+#    print(kup)
+
+    
+
+
