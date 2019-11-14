bbbd4abc42ec6ebc9715a187cdee31efdaeab893
Katre <tikukatre@gmail.com>
2019-11-14 12:23:25 +0200

    Valikud pooleli

--- test.py
+++ test.py
@@ -13,38 +13,39 @@ def kulud_tulud(a,b,c):
     for i in f:
         n=i.split(";")
         kj=int(n[0]) #kontojääk
-#    try:
-#        kj+=a
-#        uus_kontojääk=summa(kj,b)
-#        
-#    except:
-#        summa(c,b)
-#        for i in f:
-#            n=i.split(";")
-#            kj=int(n[0])
+
     kj+=a
-#        uus_kontojääk=summa(kj,b)
-#    uus_kontojääk=summa(kj,b)
-#    return uus_kontojääk
+
     return kj
+while True:
+    valik=(input("Vali tegevus: (T)äiesti uus kontojääk, (S)isesta tulud/kulud, (L)õpeta: "))
+    if valik=="T":#täiesti uue kontojäägi sisestamine faili
+        s=input("Sisestage uus kontojääk: ")
+    
+        while True: #kuupäev
+            kuupäev = input("Sisestage kuupäev formaadis pp.kk.aa: ")
+            päev,kuu,aasta = kuupäev.split('.')
+            isValidDate = True
+            try :
+                datetime.datetime(int(aasta),int(kuu),int(päev))
+                break
+            except ValueError :
+                isValidDate = False
+                print ("Kuupäev ei ole sobiv.")
+        summa(s,kuupäev)
+        continue
+    if valik=="S":
+        kt=int(input("Sisestage kulud/tulud: "))
+        uus_summa=str(kulud_tulud(kt, kuupäev,s))
 
-s=input("Sisestage kontojääk: ")
-
-while True: #kuupäev
-    kuupäev = input("Sisestage kuupäev formaadis pp.kk.aa:")
-    päev,kuu,aasta = kuupäev.split('.')
-    isValidDate = True
-    try :
-        datetime.datetime(int(aasta),int(kuu),int(päev))
-        break
-    except ValueError :
-        isValidDate = False
-        print ("Kuupäev ei ole sobiv..")
         
-kt=int(input("Sisestage kulud/tulud: "))
-summa(s,kuupäev)
-uus_summa=str(kulud_tulud(kt, kuupäev,s))
-summa(uus_summa,kuupäev)
+    if valik=="L":
+        break
+          
+#kt=int(input("Sisestage kulud/tulud: "))
+#summa(s,kuupäev)
+#uus_summa=str(kulud_tulud(kt, kuupäev,s))
+#summa(uus_summa,kuupäev)
 
 
 
