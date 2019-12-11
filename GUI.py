6a70a6d4afed8470294a5d4ead2a7cc9ab72778c
Katre <tikukatre@gmail.com>
2019-12-12 01:10:42 +0200

    GUI

--- GUI.py
+++ GUI.py
@@ -1,6 +1,63 @@
 import PySimpleGUI as sg
 import datetime
+import os
 
+#Funktsioonid
+
+def summa(a, b):
+    f = open("summa.txt", "w+")
+    
+    uf = open("sissekanded.txt", "a+")
+    
+    try:
+        f.write(a + ";" + (b))
+        print("Salvestatud")
+    except:
+        print("Ebaõnnestus")
+    f.close()
+    f = open("summa.txt", "r")
+    
+    try:  #kirjutab andmed teise faili säilitamiseks
+        andmed = f.readline()
+        uf.write(andmed +"\n")
+        uf.close()
+    except:
+        pass
+    
+def kulud_tulud(a):
+    f = open("summa.txt", "r")
+    try:
+        for i in f:
+            n = i.split(";")
+            kj = int(n[0]) #kontojääk
+        kj += a
+        return kj
+    except:
+        return("Fail on tühi")
+    
+def kuupäev(k):
+    import datetime
+    while True: #kuupäev
+        päev,kuu,aasta = k.split('.')
+        try :
+            datetime.datetime(int(aasta),int(kuu),int(päev))
+            return(k)
+        except ValueError :
+            k = input("Kuupäev ei ole sobiv. Sisestage kuupäev formaadis pp.kk.aa: ")
+            k = kuupäev(k)
+            return(k)
+def tagasta_jääk(a):
+    f = open(a, "r")
+    if os.stat(a).st_size == 0:
+        return("Konto on tühi")
+    for i in f:
+        summa_kp = i.strip().split(";")
+        return summa_kp[0]
+
+        
+    
+#GUI
+    
 sg.change_look_and_feel("Reddit")
 layout=[[sg.Text("Valige tegevus.")],
         [sg.Button("Uus kontojääk")],
@@ -47,7 +104,7 @@ while True:
     if not win2_active and event=="Tagasta kontojääk":
         win1.Hide()
         win2_active=True
-        layout4=[[sg.Text("Teie kontojääk:")],
+        layout4=[[sg.Text("Teie kontojääk:"), sg.Text((tagasta_jääk("summa.txt")))],
                   [sg.Button("Lõpeta")]]
         win4=sg.Window("Window 4").Layout(layout4)
         while True:
@@ -60,16 +117,27 @@ while True:
     if not win2_active and event=="Kustuta sissekanded":
         win1.Hide()
         win2_active=True
-        layout5=[[sg.Text("Soovite sissekanded kustutada?")],
-                  [sg.Button("Lõpeta")]]
-        win5=sg.Window("Window 5").Layout(layout5)
+        layout5=[[sg.Text("Kas olete kindel, et soovite kustutada kõik sissekanded?")],
+                 [sg.Button("Jah"), sg.Button("Ei")]]
+        win5=sg.Window("Sissekannete kustutamine").Layout(layout5)
         while True:
             event2,value2=win5.Read()
-            if event2 is None or event2==("Lõpeta"):
+            if event2 is None or event2==("Ei"):
                 win2_active=False
                 win5.Close()
                 win1.UnHide()
                 break
+            if event2=="Jah":
+                try:
+                    open("sissekanded.txt", "w").close()
+                    open("summa.txt", "w").close()
+                    sg.Popup("Edukalt kustutatud")
+                    win5.Close()
+                    win1.UnHide()
+                except:
+                    sg.Popup("Midagi läks valesti")
+                    win5.Close()
+                    win1.UnHide() 
 
         
 ##        sg.change_look_and_feel('Reddit')
